6d37e77dd0dd480ab87e5a8cee3c7df7cd204b2e
Kai <jen147774ny@gmail.com>
2025-05-03 00:07:12 +0800

    add multiple countries

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import os
 import base64
 import pytz
 
-# 從 GitHub Secrets 讀取並寫入暫存的憑證檔案
+# 從 GitHub Secrets 讀取憑證檔案
 key_base64 = os.environ.get("GCP_KEY_BASE64")
 key_path = "gcp_key.json"
 
 if not key_base64:
-    raise ValueError("❌ 沒有找到 GCP_KEY_BASE64 secret，請先在 GitHub Secrets 設定")
+    raise ValueError("沒有找到 GCP_KEY_BASE64 secret")
 
 with open(key_path, "w") as f:
     f.write(base64.b64decode(key_base64).decode("utf-8"))
@@ -18,24 +18,24 @@ with open(key_path, "w") as f:
 # 指定時區為台北
 tz = pytz.timezone("Asia/Taipei")
 tw_zone = datetime.now(tz)
-
 current_time = tw_zone.strftime("%Y-%m-%d %H:%M:%S")
 
 # 初始化 Trends 物件
 tr = Trends()
-
-# 獲取現在趨勢(24小時)
-df = tr.trending_now(geo='TW')
-
 data = []
-for item in df:
-    region=item.geo
-    keyword=item.keyword
-    volume=item.volume
-    trend_keywords = ', '.join(item.trend_keywords) if isinstance(item.trend_keywords, list) else item.trend_keywords
-    topic_names = ', '.join(item.topic_names) if isinstance(item.topic_names, list) else item.topic_names
-    volume_growth_pct=str(item.volume_growth_pct)+"%"
-    data.append([current_time,region,keyword,volume,trend_keywords,topic_names,volume_growth_pct])
+all_regions = ["TW","SG","HK","MY"]
+
+for region in all_regions:
+    # 獲取現在趨勢(24小時)
+    df = tr.trending_now(geo=region)
+    for item in df:
+        region=item.geo
+        keyword=item.keyword
+        volume=item.volume
+        trend_keywords = ', '.join(item.trend_keywords) if isinstance(item.trend_keywords, list) else item.trend_keywords
+        topic_names = ', '.join(item.topic_names) if isinstance(item.topic_names, list) else item.topic_names
+        volume_growth_pct=str(item.volume_growth_pct)+"%"
+        data.append([current_time,region,keyword,volume,trend_keywords,topic_names,volume_growth_pct])
 
 # 授權 Google Sheets
 gc = pygsheets.authorize(service_file=key_path)
